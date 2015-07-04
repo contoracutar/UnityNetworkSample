86c2f1028c1d0e2f71a760e1fe88666a5a91de6f
contoracutar <contoracutar@softbank.ne.jp>
2015-07-04 15:01:56 +0900

    替え

---
+++ MMO-shooting-do-8_01/ChatGame/Assets/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	int count = 0;
+	Vector3 vec;
+	public void Init (Vector3 v) {
+		vec = v;
+	}
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		transform.position += vec;
+
+		count++;
+		if (count > 120) {
+			Destroy (gameObject);
+		}
+		GetComponent <NetworkView> ().RPC ("MovePlayer", RPCMode.Others, transform.position);
+	}
+
+	[RPC]
+	public void MovePlayer (Vector3 position) {
+		transform.position = position;
+	}
+}
